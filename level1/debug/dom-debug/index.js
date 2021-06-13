64fdb8ed10cce391b02227fdae7356ee08999a74
Jamie Willis <jamiegalewillis@gmail.com>
2021-06-13 00:11:58 -0600

    shoplist

---
+++ level1/debug/dom-debug/index.js
@@ -0,0 +1,7 @@
+function addItem(){
+    var ul = document.getElementById('dynamic-list');
+    var candidate = document.getElementById('candidate');
+    var li = document.createElement('li');
+    li.setAttribute('id')
+}
+
