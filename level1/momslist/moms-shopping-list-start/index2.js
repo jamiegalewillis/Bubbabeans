d35f99b9a742aa78002b7a1e3c3e4238d2340a1a
jamiegalewillis <83304996+jamiegalewillis@users.noreply.github.com>
2021-06-14 15:05:39 -0600

    'dontlosethis

---
+++ level1/momslist/moms-shopping-list-start/index2.js
@@ -0,0 +1,10 @@
+//variablesconst 
+const alert = document.querySelector('.status');
+const form = document.querySelector('.form-control');
+const groceryItem =document.querySelector('grocery-item');
+const submitBtn = document.querySelector('.submit-btn')
+const list =document.querySelector('.grocey-list')
+const groceryListitems = document.querySelector('.grocery-list-item')
+
+
+//event listener
